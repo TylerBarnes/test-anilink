2153b1fd44dc32033c9d33c373d95955796e67a2
antonisierakowski <antonisierakowski@gmail.com>
2019-05-16 20:37:25 +0200

    set up the issue

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,7 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <AniLink fade to="/">Go back to the homepage</AniLink>
   </Layout>
 )
 
